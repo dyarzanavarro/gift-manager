7b6c3ae5e6ff073b674f9abd894420db2a6f954f
Dan <danielyarza1@gmail.com>
2025-12-13 17:23:13 +0100

    personmanagement start

---
+++ geschenke-manager/app/data/mockPeople.ts
@@ -0,0 +1,22 @@
+import type { Person } from '~/models/person';
+
+export const mockPeople: Person[] = [
+    {
+        id: 1,
+        name: 'Mama',
+        birthday: '1965-12-08',
+        notes: 'Mag Kochen, Wellness, keine Parfums'
+    },
+    {
+        id: 2,
+        name: 'Alex',
+        birthday: '1990-12-12',
+        notes: 'Gaming, Technik, Sneakers'
+    },
+    {
+        id: 3,
+        name: 'Maria',
+        birthday: '1988-05-04',
+        notes: 'Bücher, Pflanzen, Minimalismus'
+    }
+];
